82b580f6f8bfbd0d4fe618d50e6ed4f494791a09
Henna Maria <hennamaria2001@gmail.com>
2023-11-27 12:09:24 +0530

    note

--- src/DessertsList.js
+++ src/DessertsList.js
@@ -12,3 +12,13 @@ function DessertsList({data}) {
   }
   
   export default DessertsList;
+
+
+//   Map()
+//  allows you to create new arrays populated with the results of calling a transformation function on every element. 
+
+// Rendering lists on official React docs website
+//  dives deeper into how to display multiple similar components from a collection of data, providing examples of both filtering and transformations.
+
+// React keys on official docs
+//  offers a comprehensive set of memotecnic rules to reinforce how to use keys properly.
